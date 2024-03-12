e73948633f2a8da962f06966c05025282edcdd1a
Kirill <ekirillo@rambler.ru>
2024-03-12 13:31:46 +0500

    добавили код

---
+++ Control_work/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> FilterArray(string[] array)
+{
+    List<string> newArray = new List<string>();
+    foreach (string str in array)
+    {
+        if (str.Length <= 3)
+        {
+            newArray.Add(str);
+        }
+    }
+    return newArray;
+}
+
+string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+
+Console.WriteLine(string.Join(", ", FilterArray(array1))); 
+Console.WriteLine(string.Join(", ", FilterArray(array2))); 
+Console.WriteLine(string.Join(", ", FilterArray(array3)));  
+
